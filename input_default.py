4f8b7af22b78f30a7c1232da4bb1ab168d1e6777
t-daisuke <t.daisuke.64@gmail.com>
2023-08-05 20:53:37 +0900

    adddefault

---
+++ input_default.py
@@ -0,0 +1,11 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+N,M=map(int,input().split())
+C=input().split()
+D=input().split()
+P=[int(i) for i in input().split()]
+S=list(input())
+N=int(input())
+L=[]
+for _ in range(N):
+    L.append[int(i) for i in input().split()]
